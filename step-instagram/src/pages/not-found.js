d7f2a4a0ed7fae2f34a13109d14e43fdd2a353e7
Taras Skorohod <skorohodit12@gmail.com>
2021-10-04 19:13:38 +0300

    added page not Found

---
+++ step-instagram/src/pages/not-found.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import Header from "../components/header";
+
+export default function NotFound() {
+    useEffect(() => {
+        document.title = 'Not Found - Instagram'
+
+    },[]);
+
+    return (
+        <div className="bg-gray-background">
+            <Header />
+            <div>
+                <p className="text-center text-2xl">Not Found!</p>
+            </div>
+        </div>
+    )
+}
