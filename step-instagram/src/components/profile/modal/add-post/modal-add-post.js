747cc0fe6f0351fe4b73c616d6ee547973bb8119
SinkLine_P <popovartem505@gmail.com>
2021-10-07 21:00:38 +0300

    add Procfile

--- step-instagram/src/components/profile/modal/add-post/modal-add-post.js
+++ step-instagram/src/components/profile/modal/add-post/modal-add-post.js
@@ -1,28 +1,12 @@
 /* eslint-disable no-nested-ternary */
-import { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Box } from '@material-ui/core';
 import { Button, Input, Stack } from '@mui/material';
-import FirebaseContext from '../../../../context/firebase';
-import UserContext from '../../../../context/user';
 import './styles/modal-add-post.scss';
 
 export default function ModalAddPost({ modalOpen, closeModal }) {
-  const [comment, setComment] = useState('');
-  const { firebase, FieldValue } = useContext(FirebaseContext);
-  const {
-    user: { displayName }
-  } = useContext(UserContext);
-
-  const handleSubmitComment = (event) => {
+  const submitPost = (event) => {
     event.preventDefault();
-
-    return firebase
-      .firestore()
-      .collection('photos')
-      .update({
-        comments: FieldValue.arrayUnion({ displayName, comment })
-      });
   };
 
   return (
@@ -37,13 +21,7 @@ export default function ModalAddPost({ modalOpen, closeModal }) {
           <div>
             <p>Add Post: </p>
             <br />
-            <form
-              encType="multipart/form-data"
-              method="post"
-              onSubmit={(event) =>
-                comment.length >= 1 ? handleSubmitComment(event) : event.preventDefault()
-              }
-            >
+            <form encType="multipart/form-data" method="post">
               <p>
                 <Stack direction="row" alignItems="center" spacing={2}>
                   <Input accept="image/*" id="contained-button-file" multiple type="file" />
@@ -64,7 +42,7 @@ export default function ModalAddPost({ modalOpen, closeModal }) {
                   <Button variant="contained" color="error" onClick={closeModal}>
                     Decline
                   </Button>
-                  <Button variant="contained" color="primary" onClick={handleSubmitComment}>
+                  <Button variant="contained" color="primary" onClick={submitPost}>
                     Submit
                   </Button>
                 </Stack>
