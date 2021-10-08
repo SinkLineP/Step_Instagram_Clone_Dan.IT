8297ef5dc10dd2e7659c6a8881c8da2e20e0bd1d
SinkLine_P <popovartem505@gmail.com>
2021-10-09 01:39:56 +0300

    server

--- step-instagram/app.js
+++ step-instagram/app.js
@@ -8,15 +8,4 @@ const port = process.env.PORT || '8080';
 app.set('port', port); 
 const server = http.createServer(app);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-const prefix = process.env.NODE_ENV === 'production' ? "http://heroku_app_address" : "http://localhost:5000"
-function getUrl(relativeUrl) {
-   return prefix + "/" + relativeUrl;
-}
-
-fetch(getUrl('api/all-reviews'));
-
 server.listen(port, () => console.log(`Running on localhost:${port}`));
